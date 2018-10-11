234773a40084eeb22e789ef52a9ba9af379a6aea
sxiao1 <sxiao1@stuy.edu>
2018-10-11 19:11:41 -0400

    new Triangle

--- Triangle.java
+++ Triangle.java
@@ -2,13 +2,12 @@ public class Triangle{
   Point v1;
   Point v2;
   Point v3;
-  public Point Triangle(Point a, Point b, Point c){
+  public Triangle(Point a, Point b, Point c){
     v1 = new Point(a);
     v2 = new Point(b);
     v3 = new Point(c);
   }
-  Point [] ary = {v1, v2, v3};
-  public Point Triangle(double a, double b, double c, double d, double e, double f){
+  public Triangle(double a, double b, double c, double d, double e, double f){
     v1 = new Point(a,b);
     v2 = new Point(c,d);
     v3 = new Point(e,f);
@@ -21,9 +20,21 @@ public class Triangle{
     return perimeter;
   }
   public Point getVertex(int index){
-    if (index < 3 && index >= 0){
-      return ary[index];
+    Point v4;
+    if (index == 0) {
+      v4 = v1;
+      return v4;
     }
+    if (index == 1){
+      v4 = v2;
+      return v4;
+    }
+    if(index == 2){
+      v4 = v3;
+      return v4;
+    }
+
+    return null;
   }
   public static void setVertex(int index, Point newP){
     if (index < 3 && index >= 0){
